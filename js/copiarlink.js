f4beee88fc62ea77f1955d28e46d607f685b3968
Christian <christianmrodriguess@gmail.com>
2025-04-20 00:26:26 -0300

    20/04/25 00:26

--- js/copiarlink.js
+++
@@ -1,7 +0,0 @@
-function copiarLink(texto) {
-    navigator.clipboard.writeText(texto).then(function () {
-        alert('Link copiado: ' + texto);
-    }, function (err) {
-        console.error('Não foi possível copiar: ', err);
-    });
-}
--- js/nav-a.ativo.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll("nav a");
-const url = window.location.href;
-
-links.forEach(link => {
-  if (url.includes(link.getAttribute("href"))) {
-    link.classList.add("ativo");
-  }
-});
